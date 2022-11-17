65cabe3aaeecbe26ae79be2750f0022f8e111b2c
sanket_kothiya <sanketkothiya@gmail.com>
2022-11-17 18:56:08 +0530

    add ecommerce api in nest js but its show error

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+
+@Module({
+  imports: [ProductModule, UserModule, AuthModule, CartModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+
+console.log(process.env.JWT_SECRET);
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      // secret: process.env.JWT_SECRET,
+      secret: 'topsecret',
+
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule { }
